90f5a80f6123c2fbdf7ff7a7eea04f8aeabf6c4f
Yves Candau <ycandau@sfu.ca>
2021-10-26 21:38:06 -0700

    Separate dirname and basename for argument paths

--- routes/trees.js
+++ routes/trees.js
@@ -5,15 +5,20 @@
 const express = require('express');
 const router = express.Router();
 
+const { resolve, dirname, basename } = require('path');
+
 const getTrees = require('../helpers/fs');
 
 const [nodePath, filePath, ...dirPaths] = process.argv;
 
-const roots = dirPaths.map((path) => ({
-  name: path,
-  path: '.',
-  expandedDirs: [],
-}));
+const roots = dirPaths.map((path) => {
+  const resolvedPath = resolve(path);
+  return {
+    name: basename(resolvedPath),
+    path: dirname(resolvedPath),
+    expandedDirs: [],
+  };
+});
 
 router.get('/trees', async (req, res) => {
   const data = await getTrees(roots);
